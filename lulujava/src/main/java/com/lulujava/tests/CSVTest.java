df0d11dc13145fa65e9988160993e29a745e3bd0
marcelostockle <91279708+marcelostockle@users.noreply.github.com>
2022-01-11 15:07:09 -0300

    Create CSVTest.java
    Successful preliminary CSV test.

---
+++ lulujava/src/main/java/com/lulujava/tests/CSVTest.java
@@ -0,0 +1,25 @@
+package com.lulujava.tests;
+
+import java.io.*;
+import java.util.Iterator;
+import org.apache.commons.csv.*;
+public class CSVTest {
+    
+    public static Iterator<String> readHeaders(String filename) {
+        CSVFormat csvFormat = CSVFormat.Builder.create()
+            .setAllowMissingColumnNames(true)
+            .setHeader()
+            .setAutoFlush(true)
+            .build();
+        try {
+            Reader in = new FileReader(filename);
+            CSVParser records = csvFormat.parse(in);
+            return records.getHeaderNames().iterator();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- lulujava/src/main/java/com/lulujava/tests/Main.java
+++ lulujava/src/main/java/com/lulujava/tests/Main.java
@@ -1,13 +1,20 @@
 package com.lulujava.tests;
 
+import java.util.Iterator;
 public class Main {
     public static void main(String[] args) {
-        JSONTest();
+        CSVHeaderTest();
+        // JSONTest();
     }
     public static void JSONTest() {
         String s = JSONTest.StringTest("Example_data\\json_example.json", "settings", "id");
         System.out.println(s);
         s = JSONTest.StringTest("Example_data\\json_example.json", "settings", "value");
         System.out.println(s);
+    }
+    public static void CSVHeaderTest() {
+        Iterator<String> iter = CSVTest.readHeaders("Example_data\\otutable_test.txt");
+        while (iter.hasNext())
+            System.out.println(iter.next());
     }
 }
