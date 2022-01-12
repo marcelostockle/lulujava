a62d70fe808ac5ed7488f83ea2f8134e18458003
marcelostockle <91279708+marcelostockle@users.noreply.github.com>
2022-01-12 19:41:03 -0300

    Update CSVTest.java
    Minor fix

--- lulujava/src/main/java/com/lulujava/tests/CSVTest.java
+++ lulujava/src/main/java/com/lulujava/tests/CSVTest.java
@@ -8,6 +8,7 @@ public class CSVTest {
     public static Iterator<String> readHeaders(String filename) {
         CSVFormat csvFormat = CSVFormat.Builder.create()
             .setAllowMissingColumnNames(true)
+            .setDelimiter('\t')
             .setHeader()
             .setAutoFlush(true)
             .build();
