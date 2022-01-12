d5d2482a7259ceaa5fd92e2a929318459a6e303c
marcelostockle <91279708+marcelostockle@users.noreply.github.com>
2022-01-12 20:14:45 -0300

    Update Entry.java

--- lulujava/src/main/java/com/lulujava/lulu/Entry.java
+++ lulujava/src/main/java/com/lulujava/lulu/Entry.java
@@ -6,13 +6,20 @@ public class Entry {
     int[] otu_counts;
     String id;
     int total, spread;
+    Entry parent;
     public Entry(CSVRecord line) {
+        parent = null;
+        total = 0;
+        spread = 0;
         Iterator<String> iter = line.iterator();
-        this.id = iter.next();
-        this.otu_counts = new int[line.size() - 1];
+        id = iter.next();
+        otu_counts = new int[line.size() - 1];
         int i = 0;
         while (iter.hasNext()) {
-            this.otu_counts[i] = Integer.valueOf(iter.next());
+            otu_counts[i] = Integer.valueOf(iter.next());
+            total += otu_counts[i];
+            if (otu_counts[i] > 0)
+                spread++;
             i++;
         }
     }
