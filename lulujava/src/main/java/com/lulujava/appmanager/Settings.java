53bf4dc0fe67a161d7a18ad408c2941ab8fac9bc
marcelostockle <91279708+marcelostockle@users.noreply.github.com>
2022-01-13 20:48:15 -0300

    Update Settings.java
    Updated settings files and code to allow for extra input arguments during tests.

--- lulujava/src/main/java/com/lulujava/appmanager/Settings.java
+++ lulujava/src/main/java/com/lulujava/appmanager/Settings.java
@@ -32,9 +32,13 @@ class TestSettings {
     String in_fileA;
     String strArgA;
     String strArgB;
+    int intArgA;
+    int intArgB;
     public TestSettings(JSONObject root) {
         in_fileA = root.getString("in_fileA");
         strArgA = root.getString("strArgA");
         strArgB = root.getString("strArgB");
+        intArgA = root.getInt("intArgA");
+        intArgB = root.getInt("intArgB");
     }
 }
