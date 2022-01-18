f059e67ceff112779133994b99be67f6b00f12a4
marcelostockle <91279708+marcelostockle@users.noreply.github.com>
2022-01-18 18:49:50 -0300

    Update MatchList.java
    Added CSV output functionality

--- lulujava/src/main/java/com/lulujava/lulu/MatchList.java
+++ lulujava/src/main/java/com/lulujava/lulu/MatchList.java
@@ -2,28 +2,31 @@ package com.lulujava.lulu;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import com.lulujava.appmanager.LULUSettings;
+import java.util.ArrayList;
 import java.util.Iterator;
+import org.apache.commons.csv.CSVPrinter;
 enum AbundanceEstimator { AVG, MIN };
 public class MatchList {
     LULUSettings settings;
     OTUTable otutable;
     AbundanceEstimator abundanceEstimator;
+    final CSVFormat csvFormat = CSVFormat.Builder.create()
+        .setDelimiter('\t')
+        .setAutoFlush(true)
+        .build();
     public MatchList(LULUSettings settings, OTUTable otutable) {
         this.settings = settings;
         this.otutable = otutable;
         abundanceEstimator = AbundanceEstimator.AVG;
         if (settings.minimum_ratio_type.compareToIgnoreCase("min") == 0)
             abundanceEstimator = AbundanceEstimator.MIN;
-        CSVFormat csvFormat = CSVFormat.Builder.create()
-            .setDelimiter('\t')
-            .setAutoFlush(true)
-            .build();
         try {
             Reader in = new FileReader(settings.matchlist_file);
             CSVParser records = csvFormat.parse(in);
@@ -66,21 +69,49 @@ public class MatchList {
         else
             daughter.parent = parent.parent;
         otutable.update(daughter_key, daughter);
-        System.out.println(daughter_key + "," + daughter.parent.id);
         return true;
     }
-    private void parseResults() {
+    private void parseResults() throws IOException {      
+        CSVFormat otuMapFormat = CSVFormat.Builder.create()
+            .setDelimiter(',')
+            .setAutoFlush(true)
+            .setHeader("id", "total", "spread", "parent_id", "curated", "rank")
+            .build();
+        CSVFormat curatedTableFormat = CSVFormat.Builder.create()
+            .setDelimiter(',')
+            .setAutoFlush(true)
+            .build();
+        CSVPrinter printerOTUMap = otuMapFormat.print(
+                new FileWriter(settings.out_otu_map));
+        CSVPrinter printerCuratedTable = curatedTableFormat.print(
+                new FileWriter(settings.out_curated_table));
+        ArrayList<String> adding = new ArrayList<>(otutable.headers);
+        printerCuratedTable.printRecord(adding);
+        adding.clear();
         Iterator<Entry> iter = this.otutable.getIterator();
         int curated_count = 0;
         int discarded_count = 0;
         Entry next;
         while (iter.hasNext()) {
             next = iter.next();
-            if (next.parent == null)
+            adding.add(next.id);
+            adding.add(String.valueOf(next.total));
+            adding.add(String.valueOf(next.spread));
+            adding.add(next.getParentID());
+            if (next.parent == null) {
                 curated_count++;
-            else
+                adding.add("parent");
+                printerCuratedTable.printRecord(next.record);
+            } else {
                 discarded_count++;
+                adding.add("merged");
+            }
+            adding.add("NaN");
+            printerOTUMap.printRecord(adding);
+            adding.clear();
         }
+        printerOTUMap.close(true);
+        printerCuratedTable.close(true);
         System.out.println("curated_count = " + curated_count);
         System.out.println("discarded_count = " + discarded_count);
     }
