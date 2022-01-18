9ea114075aaf7501e2a47e4b96ac3896cda81681
marcelostockle <91279708+marcelostockle@users.noreply.github.com>
2022-01-18 18:49:02 -0300

    Updated LULUSettings.java
    Added two arguments pointing to output files.

--- lulujava/src/main/java/com/lulujava/appmanager/LULUSettings.java
+++ lulujava/src/main/java/com/lulujava/appmanager/LULUSettings.java
@@ -4,6 +4,8 @@ import org.json.JSONObject;
 public class LULUSettings {
     public String otutable_file;
     public String matchlist_file;
+    public String out_otu_map;
+    public String out_curated_table;
     public String minimum_ratio_type;
     public double minimum_ratio;
     public int minimum_match;
@@ -11,6 +13,8 @@ public class LULUSettings {
     public LULUSettings(JSONObject root) {
         otutable_file = root.getString("otutable_file");
         matchlist_file = root.getString("matchlist_file");
+        out_otu_map = root.getString("out_otu_map");
+        out_curated_table = root.getString("out_curated_table");
         minimum_ratio_type = root.getString("minimum_ratio_type");
         minimum_ratio = root.getDouble("minimum_ratio");
         minimum_match = root.getInt("minimum_match");
